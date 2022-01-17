49725405f065da5e823daea5d08cf256d3731811
Ludvig Nyqvist <ludvig.nyqvist1@outlook.com>
2022-01-17 11:02:06 +0100

    Using Fetch for GET request

--- public/javascripts/promiseAsyncAwait.js
+++ public/javascripts/promiseAsyncAwait.js
@@ -2,15 +2,9 @@
 
 console.log('Im alive, JS');
 
-let dataURL = './javascripts/cars.json';
-let res = new XMLHttpRequest();
-res.open('GET', dataURL);
-res.responseType = 'json';
-res.send();
-
-res.onload = () => {
-    const data = res.response;
+const writeList = (data) => {
     const Cars = data['Cars'];
+
     const list = document.getElementById('list'); //Gets the list element from the dom
 
     Cars.forEach(car => {
@@ -18,5 +12,34 @@ res.onload = () => {
         item.innerText = car.Make;
         list.appendChild(item);
         console.log(car.Make);
-    });
+    })
+
 }
+
+fetch('./javascripts/cars.json')
+.then(response => response.json())
+.then(data => writeList(data));
+
+// const Cars = carsData['Cars'];
+
+// console.log(Cars[0].Make);
+
+
+// let dataURL = './javascripts/cars.json';
+// let res = new XMLHttpRequest();
+// res.open('GET', dataURL);
+// res.responseType = 'json';
+// res.send();
+
+// res.onload = () => {
+//     const data = res.response;
+//     const Cars = data['Cars'];
+//     const list = document.getElementById('list'); //Gets the list element from the dom
+
+//     Cars.forEach(car => {
+//         let item = document.createElement('li');
+//         item.innerText = car.Make;
+//         list.appendChild(item);
+//         console.log(car.Make);
+//     });
+// }
